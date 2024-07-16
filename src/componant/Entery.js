57514f14f31fe01d5cdb24b575b7a5e0923c0884
yun0209 <rhgt6002g@gmail.com>
2024-07-16 12:50:49 +0900

    버튼 누르면 바디색 바뀐다.

--- src/componant/Entery.js
+++ src/componant/Entery.js
@@ -2,17 +2,22 @@ import React , {useState ,useEffect} from 'react'
 
 
 function Entery() {
-    let count = 0;
+   
+    const [count,setcount] = useState(0);
+    const bgcolor = ["red", "blue", "green", "yellow", "black","white"];
+    useEffect(()=>{
+        document.body.style.backgroundColor = bgcolor[count]
 
-    const bgcolor = ["red", "blue", "green", "yellow", "black"];
+    },[count])
 // style 은 랜더링이 아니다     
 
     return (
         <div >
-            {bgcolor.map((v,i)=>{
+            <button onClick={()=>{
+                setcount(count < bgcolor.length -1 ? count +1 : 0);
+            }}>
+                {bgcolor[count]}인디</button>
 
-            })}
-            
         </div>
         
     )
