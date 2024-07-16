9293c9b0086e2af1036d47840585d732f174f625
yun0209 <rhgt6002g@gmail.com>
2024-07-16 11:53:58 +0900

    바디 색 바꾸기

--- src/componant/Entery.js
+++ src/componant/Entery.js
@@ -4,25 +4,15 @@ import React , {useState ,useEffect} from 'react'
 function Entery() {
     let count = 0;
 
-    const [num,setnum] = useState(0);//랜더링
-
-    useEffect(()=>{
-        document.querySelector('button').addEventListener('click',()=>{
-            setnum(num + 1);
-        })
-    },[num]);//랜더링 끝나고 나서 html태그로 이해 //num 이 바뀔때 계속 돈다
-
-    useEffect(()=>{
-        document.body.style.backgroundColor ='red'
-            
-        
-    },[]);
+    const bgcolor = ["red", "blue", "green", "yellow", "black"];
+// style 은 랜더링이 아니다     
 
     return (
         <div >
-            <button>일반변수 count 하나씩 증가 </button>
-            <h1>나는 일반변수 count : {`${count = num}`}</h1>
+            {bgcolor.map((v,i)=>{
 
+            })}
+            
         </div>
         
     )
